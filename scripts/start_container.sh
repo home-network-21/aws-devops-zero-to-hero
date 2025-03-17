69b9bcda3583ff3e9f96bf877e329cae4695f1aa
home-network-21 <abouttime1021@gmail.com>
2025-03-17 18:40:28 +0530

    now

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,12 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push abouttime1021/simple-python-flask-app
+#docker push abouttime1021/simple-python-flask-app
 
 # Run the Docker image as a container in background
-docker run -d -p 5000:5000 abouttime1021/simple-python-flask-app
+#docker run -d -p 5000:5000 abouttime1021/simple-python-flask-app
+
+docker pull abhishekf5/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
