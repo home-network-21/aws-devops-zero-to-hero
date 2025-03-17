ea902213d09a715015fff8af6e32e82db54afc1b
home-network-21 <abouttime1021@gmail.com>
2025-03-18 00:30:46 +0530

    Update stops_container.sh

--- scripts/stops_container.sh
+++ scripts/stops_container.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 set -e
+
+# Stop the running container (if any)
+
+container_id=$(docker ps -q | head -n 1)
+# above command fetched the container id, now lets remove it.
+docker rm -f $container_id
